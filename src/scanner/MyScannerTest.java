bc313bd620eee4fc61700acfaaa973b7a44ac922
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-01-26 14:44:45 -0600

    updating test cases

--- src/scanner/MyScannerTest.java
+++ src/scanner/MyScannerTest.java
@@ -19,8 +19,8 @@ public class MyScannerTest {
 	        MyScanner instance = new MyScanner(new StringReader(input));
 	        
 	        
-	        //1st token 
-	        System.out.println("1st token test");
+	        //1st token(program token)
+	        System.out.println("program token test");
 	        TokenType expected = TokenType.PROGRAM;
 	        
 	        try {
@@ -30,9 +30,9 @@ public class MyScannerTest {
 	            }
 	            catch( Exception e) { e.printStackTrace();}
 	          
-	    //2nd token test 
-	        System.out.println("2nd token test");
-	        TokenType expected1 = TokenType.NUMBER;
+	    //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
 	        
 	        try {
 	            myToken = instance.nextToken();
@@ -40,6 +40,223 @@ public class MyScannerTest {
 	            System.out.println(myToken);
 	            }
 	            catch( Exception e) { e.printStackTrace();}
+	        
+	      //3rd token test(if token) 
+	        System.out.println("if token test");
+	        TokenType expected2 = TokenType.IF;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected2, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	        
+	      //4th token test(integer token) 
+	        System.out.println("integer token test");
+	        TokenType expected3 = TokenType.INTEGER;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected3, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	        
+	      //5th token test(div token) 
+	        System.out.println("div token test");
+	        TokenType expected4 = TokenType.DIV;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected4, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	        
+	      //6th token test(mod token) 
+	        System.out.println("mod token test");
+	        TokenType expected5 = TokenType.MOD;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected5, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	        
+	      //7th token test(or token) 
+	        System.out.println("or token test");
+	        TokenType expected6 = TokenType.OR;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected6, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	        
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	      //2nd token test(while token) 
+	        System.out.println("while token test");
+	        TokenType expected1 = TokenType.WHILE;
+	        
+	        try {
+	            myToken = instance.nextToken();
+	            assertEquals(expected1, myToken.getTokenType());
+	            System.out.println(myToken);
+	            }
+	            catch( Exception e) { e.printStackTrace();}
+	        
 	        
 	        }
 	    
