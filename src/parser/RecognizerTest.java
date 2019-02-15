085e482c62b83509b2d73bb246a6a6de9f464b84
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-02-14 21:50:18 -0600

    j unit recgonizer test class created

---
+++ src/parser/RecognizerTest.java
@@ -0,0 +1,22 @@
+package parser;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * This is a j unit class which will test specific functions with the Recognizer class.
+ * These functions are: program, declarations, subprogram_declaration, statement, simple_expression, factor.
+ * each test there will be 2 paths: a good pascal code- pascal is written without any errors
+ * bad pascal path - will error if the pascal code is written with errors
+ * @author Erik Ayavaca-Tirado
+ *
+ */
+
+public class RecognizerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
