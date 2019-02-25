308b50a9b47228daa89719496ab6baeea5e3e183
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-02-24 21:09:36 -0600

    added the enum class and datatype class

--- src/symbol_table/SymbolTable.java
+++ src/symbol_table/SymbolTable.java
@@ -1,14 +1,31 @@
 package symbol_table;
-import java.util.HashMap;
-
+import java.util.Hashtable;
+import java.util.Set;
 /*
- *  This is a java class, that wi
+ *  This is a java class, that will be using a hashmap to keep track 
+ *  of different id types such as array,function, variable and program ids.
+ *  They appear in the object form of(String, DataType)
  * 
- * 
- * 
+ * @author Erik Ayavaca-Tirado
  */
 public class SymbolTable {
 	
+	private Hashtable<String , DataType> symbols = new Hashtable<String, DataType>();
 	
+	
+	/**
+	 * Enum to track the different types of symbols that
+	 * can be stored in the symboltable
+	 */
+	private enum Kind{
+		PROGRAM,VARIABLE,ARRAY,FUNCTION,PROCEDURE
+		
+	}
+	
+	private class DataType{
+		public String name;
+		public Kind kind;
+		public String type;	
+	}
 
 }
