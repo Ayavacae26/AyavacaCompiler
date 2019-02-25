541649b86369533c536932e72cc76801e72756da
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-02-24 21:36:00 -0600

    finished with add variable function

--- src/symbol_table/SymbolTable.java
+++ src/symbol_table/SymbolTable.java
@@ -1,5 +1,5 @@
 package symbol_table;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.Set;
 /*
  *  This is a java class, that will be using a hashmap to keep track 
@@ -10,7 +10,7 @@ import java.util.Set;
  */
 public class SymbolTable {
 	
-	private Hashtable<String , DataType> symbols = new Hashtable<String, DataType>();
+	private HashMap<String , DataType> symbols = new HashMap<String, DataType>();
 	
 	
 	/**
@@ -26,6 +26,44 @@ public class SymbolTable {
 		public String name;
 		public Kind kind;
 		public String type;	
+	}
+	
+	/**
+	 * This method addVariable checks to see if there exists a name
+	 * of the type variable, returns false if found, else it add's in to the hashmap
+	 * @param name- name of variable that is being checked by hashmap
+	 * @return True if there is no existance of the name.
+	 */
+	
+	public boolean addVariable(String name) {
+		DataType ss = new DataType();
+	if (symbols.containsKey(name)) {
+		return false;
+	}
+	else {
+		ss.name = name;
+		ss.kind = Kind.VARIABLE;
+		symbols.put(name, ss);
+		System.out.println("Adding " + name + " in type " + ss.kind + " to hashtable");
+		return true;
+	}
+	}
+	
+	/**
+	 * This isVariable method checks to see if the kind of id matches
+	 * the enum type of variable 
+	 * @param name -The variable id name that is being checked in the hash map
+	 * @return True if there is a match 
+	 */
+	public boolean isVariable(String name) {
+		DataType data = (DataType) symbols.get(name);
+		if(data == null) {
+			return false;
+		}
+		else if(data.kind == Kind.VARIABLE){
+			return true;
+		}
+		return false;
 	}
 
 }
