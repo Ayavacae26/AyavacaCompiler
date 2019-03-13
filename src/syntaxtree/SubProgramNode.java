885b7040855d608aaa30173a1fd8b8ddb40648ed
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-03-13 15:28:51 -0500

    add this so the Subprogramdeclationsnode java  works

---
+++ src/syntaxtree/SubProgramNode.java
@@ -0,0 +1,19 @@
+package syntaxtree;
+
+/**
+ * this represents a SubProgramNode in a syntax tree
+ * @author erikayavaca
+ *
+ */
+public class SubProgramNode extends SyntaxTreeNode{
+	/**
+	 * Creates a string representation of the SubprogramNode
+	 * @param level -the tree level at which this node resides 
+	 * @return A string representing this node. 
+	 */
+	@Override
+	public String indentedToString(int level) {
+		return null;
+	}
+
+}
