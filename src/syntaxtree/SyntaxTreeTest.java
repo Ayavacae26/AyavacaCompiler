1b12b8e927e807a5dd00f8eb1cdbc75b48a10ff4
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-03-14 11:27:02 -0500

    test ius working as it should

--- src/syntaxtree/SyntaxTreeTest.java
+++ src/syntaxtree/SyntaxTreeTest.java
@@ -88,23 +88,23 @@ public class SyntaxTreeTest {
 
 		assertEquals(expected, actual);
 	}
-	/*
-	 * @Test public void programTest() { Parser parser = new Parser(
-	 * "money.txt",true); ProgramNode actual = parser.program(); String actualString
-	 * = actual.indentedToString( 0); String expectedString =
-	 * 
-	 * "Program: sample\n" + "|-- Declarations\n" + "|-- --- Name: dollars\n" +
-	 * "|-- --- Name: yen\n" + "|-- --- Name: bitcoins\n" +
-	 * "|-- SubProgramDeclarations\n" + "|-- Compound Statement\n" +
-	 * "|-- --- Assignment\n" + "|-- --- --- Name: dollars\n" +
-	 * "|-- --- --- Value: 1000000\n" + "|-- --- Assignment\n" +
-	 * "|-- --- --- Name: yen\n" + "|-- --- --- Operation: MULTIPLY\n" +
-	 * "|-- --- --- --- Name: dollars\n" + "|-- --- --- --- Value: 110\n" +
-	 * "|-- --- Assignment\n" + "|-- --- --- Name: bitcoins\n" +
-	 * "|-- --- --- Operation: DIVIDE\n" + "|-- --- --- --- Name: dollars\n" +
-	 * "|-- --- --- --- Value: 3900\n";
-	 * 
-	 * 
-	 * assertEquals( expectedString, actualString); }
-	 */
+	
+	// curently not finding the file for some reason 
+	 /* @Test
+	  public void programTest() { 
+		 String filename = "Money.txt";
+		 Parser parser = new Parser(filename,true); 
+		 ProgramNode actual = parser.program(); 
+		 String actualString = actual.indentedToString( 0); 
+		 String expectedString = "Program: sample\n" + "|-- Declarations\n" + "|-- --- Name: dollars\n" + "|-- --- Name: yen\n"
+						+ "|-- --- Name: bitcoins\n" + "|-- SubProgramDeclarations\n" + "|-- Compound Statement\n"
+						+ "|-- --- Assignment\n" + "|-- --- --- Name: dollars\n" + "|-- --- --- Value: 1000000\n"
+						+ "|-- --- Assignment\n" + "|-- --- --- Name: yen\n" + "|-- --- --- Operation: ASTERISK\n"
+						+ "|-- --- --- --- Name: dollars\n" + "|-- --- --- --- Value: 110\n" + "|-- --- Assignment\n"
+						+ "|-- --- --- Name: bitcoins\n" + "|-- --- --- Operation: SLASH\n" + "|-- --- --- --- Name: dollars\n"
+						+ "|-- --- --- --- Value: 3900\n";
+				 assertEquals( expectedString, actualString);
+			
+		 }*/
+	 
 }
