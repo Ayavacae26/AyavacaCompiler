53332a9db2cc639bc441eb66a623ca83a1653dcc
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-03-13 21:54:43 -0500

    junit test class for syntax tree

---
+++ src/syntaxtree/SyntaxTreeTest.java
@@ -0,0 +1,14 @@
+package syntaxtree;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SyntaxTreeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
