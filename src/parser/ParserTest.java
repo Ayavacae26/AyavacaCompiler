30011baa480d54e3f955c1b1073894d2be9fd4c3
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-04-11 15:59:36 -0500

    created the paser test file

---
+++ src/parser/ParserTest.java
@@ -0,0 +1,24 @@
+package parser;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import parser.Parser;
+import symboltable.SymbolTable;
+import syntaxtree.*;
+
+/**
+ * This is a junit test class testing the parser
+ * we are going to be testing factor(), simple_expression(), 
+ * statement(), subprogram_declaration(), declarations(), and program().
+ * @author erikayavaca
+ *
+ */
+
+public class ParserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
