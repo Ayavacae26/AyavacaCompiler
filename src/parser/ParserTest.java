349e4ccf94ef126ab0e25227119f1f67f89cacba
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-04-11 17:40:00 -0500

    added the 6 different test cases

--- src/parser/ParserTest.java
+++ src/parser/ParserTest.java
@@ -16,9 +16,56 @@ import syntaxtree.*;
 
 public class ParserTest {
 
+	/**
+	 * This test is going to be testing the factor function within the parser
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented"); // TODO
+	public void factorTest() {
+		Parser par = new Parser("7", false);
+		
 	}
+	/**
+	 * This test is going to be testing the simple_expression function within the parser
+	 */
+	@Test
+	public void simple_expressionTest() {
+		Parser par = new Parser("7", false);
+		
+	}
+	/**
+	 * This test is going to be testing the statement function within the parser
+	 */
+	@Test
+	public void statementTest() {
+		Parser par = new Parser("7", false);
+		
+	}
+	
+	/**
+	 * This test is going to be testing the subprogram_declaration function within the parser
+	 */
+	@Test
+	public void subprogram_declarationTest() {
+		Parser par = new Parser("7", false);
+		
+	}
+	
+	/**
+	 * This test is going to be testing the declarations function within the parser
+	 */
+	@Test
+	public void declarationsTest() {
+		Parser par = new Parser("7", false);
+		
+	}
+	
 
+	/**
+	 * This test is going to be testing the program function within the parser
+	 */
+	@Test
+	public void programTest() {
+		Parser par = new Parser("7", false);
+		
+	}
 }
