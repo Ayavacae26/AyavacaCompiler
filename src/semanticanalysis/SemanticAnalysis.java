9ff1dd4ce877762df066dc76e1bc8ad72a7f1f35
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2019-04-24 23:40:51 -0500

    created the semantic analysis package and jave file

---
+++ src/semanticanalysis/SemanticAnalysis.java
@@ -0,0 +1,16 @@
+package semanticanalysis;
+
+
+/**
+ * In this following class the following must be achived 
+ * Make sure all variables are declared before they are used.
+ * Assign a datatype, integer or real, to each expression (declare a type field in ExpressionNode, for example). ---
+ * The type of any expression node in the tree should be printed in the tree's indentedToString.
+ * Make sure that types match across assignment.
+ * 
+ * @author erikayavaca
+ * 
+ */
+public class SemanticAnalysis {
+
+}
